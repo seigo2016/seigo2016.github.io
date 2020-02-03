00d07dfb697aad7f9741983c3d3f77901d2f15e7
seigo2016 <2003seigo@gmail.com>
2020-02-03 15:53:30 +0900

    fix load speed

--- static/js/main.js
+++ static/js/main.js
@@ -2,15 +2,12 @@ function OnButtonClick(x) {
     $('.ui.modal.'+x).modal('show');
 }
 $('.ui.embed').embed();
-window.onload = function(){
-    function youtube_defer() {
-        var iframes = document.querySelectorAll('.youtube');
-        iframes.forEach(function(iframe){
-        if(iframe.getAttribute('data-src')) {
-            iframe.setAttribute('src',iframe.getAttribute('data-src'));
-        }
-        });
+function youtube_defer() {
+    var iframes = document.querySelectorAll('.youtube');
+    iframes.forEach(function(iframe){
+    if(iframe.getAttribute('data-src')) {
+        iframe.setAttribute('src',iframe.getAttribute('data-src'));
     }
-    window.addEventListener('load', youtube_defer);
-
+    });
 }
+window.addEventListener('load', youtube_defer);
